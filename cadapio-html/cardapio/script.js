15470641d13649c2c1ef214db449055f5ad32669
euquias <willmagalhaes25@gmail.com>
2022-09-10 01:22:07 -0300

    atualizaçao cardapio

--- cadapio-html/cardapio/script.js
+++ cadapio-html/cardapio/script.js
@@ -3,11 +3,13 @@
       const cardapio = document.getElementById('cardapio')
       e.preventDefault()
       console.log('afsdfasdf')
-      fetch(link.href)
+       fetch(link.href)
         .then(resp => resp.text())
-        .then(html => cardapio.innerHTML = html)
+        .then(html => cardapio.innerHTML = html) 
 
     }
   }) 
-  
+  const a = 2022
+
+  console.log(a)
   
